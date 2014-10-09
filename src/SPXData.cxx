c1064af6249d3b0987b68ab918882f34adffe0ef
Joseph Gibson <gibsjose@mail.gvsu.edu>
2014-10-09 14:17:11 +0200

    Added ::Parse and ::Print for Spectrum T1A

--- src/SPXData.cxx
+++ src/SPXData.cxx
@@ -173,7 +173,95 @@ void SPXData::ParseSpectrumT1S(void) {
 }
 
 void SPXData::ParseSpectrumT1A(void) {
+
 	std::string mn = "ParseSpectrumT1A: ";
+
+	if(debug) std::cout << cn << mn << "Beginning to parse data file: " << dataSteeringFile.GetDataFile() << std::endl;
+
+	if(!(*dataFile)) {
+		throw SPXFileIOException("Something went awry with the dataFile ifstream...");
+	}
+
+	std::string line;
+	double xm_t, xlow_t, xhigh_t, sigma_t, stat_t, syst_p_t, syst_n_t;
+	std::vector<double> xm;				//Mean x
+	std::vector<double> xlow;			//X min
+	std::vector<double> xhigh;			//X max
+	std::vector<double> sigma;			//Sigma (cross-section)
+	std::vector<double> stat;			//Statistical error
+	std::vector<double> syst_p;			//Total systematic error (+)
+	std::vector<double> syst_n;			//Total systematic error (-)
+
+	while(dataFile->good()) {
+		std::getline(*dataFile, line);
+		
+		//String stream to parse the individual lines
+		std::istringstream iss(line);
+
+		if(debug) std::cout << cn << mn << "Line: " << line << std::endl;
+
+		//Skip empty lines
+		if(line.empty()) {
+			continue;
+		}
+
+		//Skip comments
+		if(!line.empty() && (line[0] == ';')) {
+			continue;
+		} else if(!line.empty()) {
+
+			//Parse each line into temp variables
+			iss >> xm_t >> xlow_t >> xhigh_t >> sigma_t >> stat_t >> syst_p_t >> syst_n_t;
+
+			//Fill vectors with temp variables
+			xm.push_back(xm_t);
+			xlow.push_back(xlow_t);
+			xhigh.push_back(xhigh_t);
+			sigma.push_back(sigma_t);
+			stat.push_back(stat_t);
+			syst_p.push_back(syst_p_t);
+			syst_n.push_back(syst_n_t);
+		}
+	}
+
+	//Check vector sizes: all vectors should be the same size
+	int masterSize = xm.size();
+	if(debug) std::cout << cn << mn << "Master size set to size of \"xm\" vector: " << masterSize << std::endl;
+
+	if(debug) std::cout << cn << mn << "Checking sizes of all other vectors.." << std::endl;
+
+	try {
+		CheckVectorSize(xlow, "xlow", masterSize);
+		CheckVectorSize(xhigh, "xhigh", masterSize);
+		CheckVectorSize(sigma, "sigma", masterSize);
+		CheckVectorSize(stat, "stat", masterSize);
+		CheckVectorSize(syst_p, "syst_p", masterSize);
+		CheckVectorSize(syst_n, "syst_n", masterSize);
+	} catch(const SPXException &e) {
+		throw;
+	}
+
+	//All vectors passed size check
+	if(debug) std::cout << cn << mn << "Success: All vector sizes match master size" << std::endl;
+
+	//Set numberOfBins based on master size
+	if(debug) std::cout << cn << mn << "Setting numberOfBins to match master size" << std::endl;
+	numberOfBins = masterSize;
+	if(debug) std::cout << cn << mn << "numberOfBins = " << numberOfBins << std::endl;
+
+	//Add to data map
+	data.insert(std::pair<std::string, std::vector<double> >("xm", xm));
+	data.insert(std::pair<std::string, std::vector<double> >("xlow", xlow));
+	data.insert(std::pair<std::string, std::vector<double> >("xhigh", xhigh));
+	data.insert(std::pair<std::string, std::vector<double> >("sigma", sigma));
+	data.insert(std::pair<std::string, std::vector<double> >("stat", stat));
+	data.insert(std::pair<std::string, std::vector<double> >("syst_p", syst_p));
+	data.insert(std::pair<std::string, std::vector<double> >("syst_n", syst_n));
+
+	//Print formatted table
+	if(debug) {
+		PrintSpectrumT1A();
+	}
 }
 
 void SPXData::ParseSpectrumT2S(void) {
@@ -218,8 +306,13 @@ void SPXData::PrintSpectrumT1S(void) {
 	for(int i = 0; i < numberOfBins; i++) {
 		//std::pair<std::string, std::vector<double> > p;
 
+		//Show 4 decimal places
+		std::cout << std::fixed;
+		std::cout.precision(4);
+
 		std::cout << "| ";
-		std::cout.width(10); std::cout << data["xm"][i];
+		std::cout.width(10); 
+		std::cout << data["xm"][i];
 		std::cout << " | ";
 		std::cout.width(10); std::cout << data["xlow"][i];
 		std::cout << " | ";
@@ -233,11 +326,44 @@ void SPXData::PrintSpectrumT1S(void) {
 		std::cout << " |" << std::endl;
 	}
 
-	std::cout << "===============================================================================" << std::endl;
+	std::cout << "===============================================================================" << std::endl << std::endl;
 }
 
 void SPXData::PrintSpectrumT1A(void) {
 
+	std::cout << std::endl << "Spectrum T1A Data File: " << dataSteeringFile.GetDataFile() << std::endl << std::endl;
+
+	std::cout << "============================================================================================" << std::endl;
+	std::cout << "|         xm |       xlow |      xhigh |      sigma |       stat |     syst + |     syst - |" << std::endl;
+	std::cout << "--------------------------------------------------------------------------------------------" << std::endl;
+
+	//Iterate over map
+	for(int i = 0; i < numberOfBins; i++) {
+		//std::pair<std::string, std::vector<double> > p;
+
+		//Show 4 decimal places
+		std::cout << std::fixed;
+		std::cout.precision(4);
+
+		std::cout << "| ";
+		std::cout.width(10); 
+		std::cout << data["xm"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["xlow"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["xhigh"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["sigma"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["stat"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["syst_p"][i];
+		std::cout << " | ";
+		std::cout.width(10); std::cout << data["syst_n"][i]; 
+		std::cout << " |" << std::endl;
+	}
+
+	std::cout << "============================================================================================" << std::endl;
 }
 
 void SPXData::PrintSpectrumT2S(void) {
