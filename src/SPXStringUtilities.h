1886d0265a5fef51e1683714c7d6caaa643f2b78
Joseph Gibson <gibsjose@mail.gvsu.edu>
2014-10-13 11:20:15 +0200

    Branching out Utilities class into sub-classes

---
+++ src/SPXStringUtilities.h
@@ -0,0 +1,62 @@
+//************************************************************/
+//
+//	String Utilities Header
+//
+//	Some useful string utilities for Spectrum
+//
+//	@Author: 	Joe Gibson - CERN ATLAS
+//	@Date:		13.10.2014
+//
+//************************************************************/
+
+#ifndef SPXSTRINGUTILITIES_H
+#define SPXSTRINGUTILITIES_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+
+class SPXStringUtilities {
+
+public:
+	static std::vector<std::string> ParseString(std::string rawData, char delimeter) {
+	  std::stringstream lineStream(rawData);
+	  std::string cell;
+	  std::vector<std::string> parsedDataVec;
+	  parsedDataVec.clear();
+
+	  while(getline(lineStream,cell,delimeter)) {
+		cell.erase( std::remove(cell.begin(), cell.end(), ' '), cell.end() ); //remove any whitespace
+		parsedDataVec.push_back(cell);
+	  }
+		
+	  if(parsedDataVec.size()==0) parsedDataVec.push_back(rawData);    
+		
+	  return parsedDataVec;
+	}
+
+	static std::vector<std::string> CommaSeparatedListToVector(const std::string &csl) {
+	  	return ParseString(csl, ',');
+	}
+
+	static std::string VectorToCommaSeparatedList(std::vector<std::string> &v) {
+		std::string csl;
+		
+		csl.clear();
+	 	
+	 	for(std::vector<std::string>::iterator it = v.begin(); it != v.end(); ++it) {
+	 		csl += *it;
+	 		
+	 		if((it + 1) != v.end()) {
+	 			csl += ", ";
+	 		}
+	 	}
+	 	
+	 	return csl;
+	}
+};
+
+#endif
--- src/SPXUtilities.h
+++ src/SPXUtilities.h
@@ -2,7 +2,7 @@
 //
 //	Spectrum Utilities Header
 //
-//	Some usefull file utilities, string utilities, etc.
+//	Simply includes all the other utility classes
 //
 //	@Author: 	Joe Gibson - CERN ATLAS
 //	@Date:		25.09.2014
@@ -12,51 +12,8 @@
 #ifndef SPXUTILITIES_H
 #define SPXUTILITIES_H
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <algorithm>
-
-
-class SPXUtilities {
-
-public:
-	static std::vector<std::string> ParseString(std::string rawData, char delimeter) {
-	  std::stringstream lineStream(rawData);
-	  std::string cell;
-	  std::vector<std::string> parsedDataVec;
-	  parsedDataVec.clear();
-
-	  while(getline(lineStream,cell,delimeter)) {
-		cell.erase( std::remove(cell.begin(), cell.end(), ' '), cell.end() ); //remove any whitespace
-		parsedDataVec.push_back(cell);
-	  }
-		
-	  if(parsedDataVec.size()==0) parsedDataVec.push_back(rawData);    
-		
-	  return parsedDataVec;
-	}
-
-	static std::vector<std::string> CommaSeparatedListToVector(const std::string &csl) {
-	  	return ParseString(csl, ',');
-	}
-
-	static std::string VectorToCommaSeparatedList(std::vector<std::string> &v) {
-		std::string csl;
-		
-		csl.clear();
-	 	
-	 	for(std::vector<std::string>::iterator it = v.begin(); it != v.end(); ++it) {
-	 		csl += *it;
-	 		
-	 		if((it + 1) != v.end()) {
-	 			csl += ", ";
-	 		}
-	 	}
-	 	
-	 	return csl;
-	}
-};
+#include "SPXStringUtilities.h"
+//#include "SPXMathUtilities.h"
+//#include "SPXGraphUtilities.h"
 
 #endif
