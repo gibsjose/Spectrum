fa74c3a7e2424418e298bb1ad4091b0dcaeb23e1
Joseph Gibson <gibsjose@mail.gvsu.edu>
2014-10-13 11:11:59 +0200

    Wrote templates for Initialization and Plotting. Wrote dummy method for plotting one data set

--- src/SPXAnalysis.h
+++ src/SPXAnalysis.h
@@ -15,14 +15,54 @@
 #ifndef SPXANALYSIS_H
 #define SPXANALYSIS_H
 
+//#include "SPXPlot.h"
+#include "SPXData.h" //TEMPORARY!
+#include "SPXSteeringFile.h"
+#include "SPXException.h"
+
 class SPXAnalysis {
 
 public:
-	SPXAnalysis (void) {}
-	explicit SPXAnalysis(SPXSteeringFile &steeringFile) : this->steeringFile(steeringFile) {}
+	explicit SPXAnalysis(SPXSteeringFile *sf) : steeringFile(*sf) {}
 
-	void Initialize(void) {}
-	void Plot(void) {}
+	void Initialize(void) {
+		/*
+		try {
+			for(int i = 0; i < steeringFile.GetNumberOfPlots(); i++) {
+				SPXPlot plot = SPXPlot();
+				plot.Initilize();
+				plots.push_back(plot);
+			}
+		} catch(const SPXException &e) {
+			throw;
+		}
+		*/
+	}
+
+	void Plot(void) {
+		//TEMPORARY!!!
+		SPXData::SetDebug(true);
+		SPXFrameOptionsInstance &foi = steeringFile.GetFrameOptionsInstance(0, 0);
+		SPXData data = SPXData(foi);
+
+		try {
+			data.Parse();	//Parse the data
+			data.Print();	//Print the data to the console
+			data.Draw();	//Draw the data in a frame
+		} catch(const SPXException &e) {
+			throw;
+		}
+
+		/*
+		try {
+			for(int i = 0; i < plots.size(); i++) {
+				plots[i].Plot();
+			}
+		} catch(const SPXException &e) {
+			throw;
+		}
+		*/
+	}
 	
 	static bool GetDebug(void) {
 		return debug;
@@ -35,7 +75,7 @@ public:
 private:
 	static bool debug;					//Flag indicating debug mode
 	SPXSteeringFile &steeringFile;		//Reference to a fully parsed steering file	
-	std::vector<SPXPlot> plots;			//Vector of plots
+	//std::vector<SPXPlot> plots;			//Vector of plots
 };
 
 #endif
