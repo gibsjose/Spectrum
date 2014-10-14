bc29e1aa1da8594aa1236842fcfd0176fa5ed8d8
Joseph Gibson <gibsjose@mail.gvsu.edu>
2014-10-14 14:55:33 +0200

    Created SPXCrossSection class

---
+++ src/SPXCrossSection.cxx
@@ -0,0 +1,21 @@
+//************************************************************/
+//
+//	CrossSection Implementation
+//
+//	Implements the SPXCrossSection class, which performs the
+//	convolution and all math required to calculate the cross-
+//	section from a PDF and a grid file.
+//
+//	@Author: 	J. Gibson, C. Embree, T. Carli - CERN ATLAS
+//	@Date:		14.10.2014
+//
+//************************************************************/
+
+#include "SPXCrossSection.h"
+#include "SPXUtilities.h"
+
+//Class name for debug statements
+const std::string cn = "SPXCrossSection::";
+
+//Must define the static debug variable in the implementation
+bool SPXCrossSection::debug;
