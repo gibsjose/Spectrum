402cdda05240c68336176dcc0cb4af9ed7c660c2
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-10-22 15:23:43 +0200

    Added SPXROOTException

--- src/SPXException.h
+++ src/SPXException.h
@@ -28,23 +28,23 @@
 class SPXException : public std::runtime_error {
 public:
 	SPXException() : std::runtime_error("Unknown Exception") {}
-	
+
 	virtual ~SPXException() throw() {}
 
 	virtual const char * what() const throw() {
 		return "SPXException: Unknown Exception";
 	}
-	
+
 	/*
 	//@TODO
 	virtual const char * what() const throw() {
 		std::string detailedMessage;
-		
+
 		detailedMessage = filename + ": " + lineNumber + ": (" + className + "::" + methodName + ": " + message;
 		return detailedMessage.c_str();
 	}
 	*/
-	
+
 private:
 	std::string message;
 	std::string className;
@@ -58,43 +58,43 @@ public:
 	SPXGeneralException(std::string message) : SPXException() {
 		this->message = message;
 	}
-	
+
 	~SPXGeneralException() throw() {}
-	
+
 	const char * what() const throw() {
 		std::string tmp;
 
 		tmp = "SPXGeneralException: " + message;
-		
+
 		return tmp.c_str();
 	}
-	
+
 private:
 	std::string message;
 };
 
 class SPXINIParseException : public SPXException {
-public:	
+public:
 	SPXINIParseException(std::string section, std::string name, std::string message) : SPXException() {
 		this->section = section;
 		this->name = name;
 		this->message = message;
 	}
-	
+
 	SPXINIParseException(std::string section, std::string message) : SPXException() {
 		this->section = section;
 		this->name.clear();
 		this->message = message;
 	}
-	
+
 	~SPXINIParseException() throw() {}
-	
+
 	const char * what() const throw() {
 		std::string tmp;
 		tmp = "SPXINIParseException: Error parsing [" + section + "]:" + name + ": " + message;
 		return tmp.c_str();
 	}
-	
+
 private:
 	std::string section;
 	std::string name;
@@ -112,9 +112,9 @@ public:
 		this->filename.clear();
 		this->message = message;
 	}
-	
+
 	~SPXFileIOException() throw() {}
-	
+
 	const char * what() const throw() {
 		std::string tmp;
 		if(!filename.empty()) {
@@ -122,10 +122,10 @@ public:
 		} else {
 			tmp = "SPXFileIOException: " + message;
 		}
-		
+
 		return tmp.c_str();
 	}
-	
+
 private:
 	std::string filename;
 	std::string message;
@@ -137,26 +137,26 @@ public:
 		this->filename.clear();
 		this->message = message;
 	}
-	
+
 	SPXParseException(std::string filename, std::string message) : SPXException() {
 		this->filename = filename;
 		this->message = message;
 	}
-	
+
 	~SPXParseException() throw() {}
-	
+
 	const char * what() const throw() {
 		std::string tmp;
-		
+
 		if(filename.empty()) {
 			tmp = "SPXParseException: " + message;
 		} else {
 			tmp = "SPXParseException: File: " + filename + ": " + message;
 		}
-		
+
 		return tmp.c_str();
 	}
-	
+
 private:
 	std::string filename;
 	std::string message;
@@ -170,24 +170,24 @@ public:
 		this->val = val;
 		this->message = message;
 	}
-	
+
 	SPXOutOfRangeException(int top, int val, std::string message) : SPXException() {
 		this->bottom = 0;
 		this->top = top;
 		this->val = val;
 		this->message = message;
 	}
-	
+
 	~SPXOutOfRangeException() throw() {}
-	
+
 	const char * what() const throw() {
 
 		std::ostringstream s;
 		s << "SPXOutOfRangeException: Value: " << val << " outside of range: " << bottom << " <--> " << top << ": " << message;
-		
+
 		return s.str().c_str();
 	}
-	
+
 private:
 	int bottom;
 	int top;
@@ -200,19 +200,38 @@ public:
 	SPXGraphException(std::string message) : SPXException() {
 		this->message = message;
 	}
-	
+
 	~SPXGraphException() throw() {}
-	
+
 	const char * what() const throw() {
 		std::string tmp;
 
 		tmp = "SPXGraphException: " + message;
-		
+
 		return tmp.c_str();
 	}
-	
+
 private:
-	std::string filename;
+	std::string message;
+};
+
+class SPXROOTException : public SPXException {
+public:
+	SPXROOTException(std::string message) : SPXException() {
+		this->message = message;
+	}
+
+	~SPXROOTException() throw() {}
+
+	const char * what() const throw() {
+		std::string tmp;
+
+		tmp = "SPXROOTException: " + message;
+
+		return tmp.c_str();
+	}
+
+private:
 	std::string message;
 };
 
