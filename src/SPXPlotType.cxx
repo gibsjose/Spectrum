7a8df7a5cc2c2ed6ad860fd331095fb670e152eb
Joseph Gibson <gibsjose@mail.gvsu.edu>
2014-10-27 15:46:16 +0100

    Added SPXPlotType class

---
+++ src/SPXPlotType.cxx
@@ -0,0 +1,195 @@
+//************************************************************/
+//
+//	Plot Type Implementation
+//
+//	Implements the SPXPlotType class, which defines the four
+//	supported plot types:
+//		
+//		1)	data, grid, pdf
+//
+//			ONE data file
+//			ONE grid file
+//			ONE pdf file
+//
+//		2)	data[], grid[], pdf
+//
+//			MANY data files
+//			MANY grid files (1:1 w/ data files)
+//			ONE pdf file
+//
+//		3) 	data, grid, pdf[]
+//
+//			ONE data file
+//			ONE grid file
+//			MANY pdf files
+//
+//		4)	data, grid[], pdf
+//
+//			ONE data file
+//			MANY grid files
+//			ONE pdf file
+//
+//	@Author: 	J. Gibson, C. Embree, T. Carli - CERN ATLAS
+//	@Date:		27.10.2014
+//	@Email:		gibsjose@mail.gvsu.edu
+//
+//************************************************************/
+
+#include <iostream>
+#include <sstream>
+
+#include "SPXPlotType.h"
+#include "SPXUtilities.h"
+#include "SPXException.h"
+
+//Class name for debug statements
+const std::string cn = "SPXPlotType::";
+
+//Must define the static debug variable in the implementation
+bool SPXPlotType::debug;
+
+//Constructs a SPXPlotType object with a given string (effectively
+//	calls the Parse() method on the input string)
+SPXPlotType::SPXPlotType(std::string s) {
+	this->Clear();
+	this->Parse(s);
+}
+
+//Sets the type based on the input string
+void SPXPlotType::Parse(std::string s) {
+	std::string mn = "Parse: ";
+	
+	if(debug) std::cout << cn << mn << "Parsing configuration string: " << s << std::endl;
+	
+	//Clear the type each time it is parsed
+	this->Clear();
+	
+	//Parse the string into type options
+	std::vector<std::string> v = SPXStringUtilities::CommaSeparatedListToVector(s);
+	
+	if(debug) {
+		std::cout << cn << mn << "Configuration string: " << s << " was parsed into:" << std::endl;
+		for(int i = 0; i < v.size(); i++) {
+			std::cout << "\t" << v[i] << std::endl;
+		}	
+		std::cout << std::endl;
+	}
+	
+	//Check the parsed type options vector for size errors
+	if(v.size() != 3) {
+		type = DS_INVALID;
+		throw SPXParseException("Incorrect plot type: Configuration string MUST be a combination of ALL three: \"data\", grid, and \"pdf\"");
+	}
+
+	std::string data = v.at(0);
+	std::string grid = v.at(1);
+	std::string pdf = v.at(2);
+	
+	//Parse data
+	if(!data.compare("data")) {
+		if(debug) std::cout << cn << mn << "Successfully matched \"data\"" << std::endl;
+		type &= ~PT_DATA;
+	}
+	else if(!data.compare("data[]")) {
+		if(debug) std::cout << cn << mn << "Successfully matches \"data[]\"" << std:endl;
+		type |= PT_DATA;
+	}
+	else {
+		throw SPXParseException("Incorrect plot type: Data configuration string: Unrecognized option: " + data + ": MUST be either \"data\" or \"data[]\"");
+	}
+
+	//Parse grid
+	if(!grid.compare("grid")) {
+		if(debug) std::cout << cn << mn << "Successfully matched \"grid\"" << std::endl;
+		type &= ~PT_GRID;
+	}
+	else if(!grid.compare("grid[]")) {
+		if(debug) std::cout << cn << mn << "Successfully matches \"grid[]\"" << std:endl;
+		type |= PT_GRID;
+	}
+	else {
+		throw SPXParseException("Incorrect plot type: Grid configuration string: Unrecognized option: " + grid + ": MUST be either \"grid\" or \"grid[]\"");
+	}
+
+	//Parse pdf
+	if(!pdf.compare("pdf")) {
+		if(debug) std::cout << cn << mn << "Successfully matched \"pdf\"" << std::endl;
+		type &= ~PT_PDF;
+	}
+	else if(!pdf.compare("pdf[]")) {
+		if(debug) std::cout << cn << mn << "Successfully matches \"pdf[]\"" << std:endl;
+		type |= PT_PDF;
+	}
+	else {
+		throw SPXParseException("Incorrect plot type: PDF configuration string: Unrecognized option: " + pdf + ": MUST be either \"pdf\" or \"pdf[]\"");
+	}
+
+	//Check for incorrect combinations
+	if(!IsValid()) {
+		throw SPXParseException("Incorrect plot type: The combination: " << this->ToString() << " is invalid" << std::endl;
+	}
+
+}
+
+//Print displays the output of ToString to the console
+void SPXPlotType::Print(void) {
+	std::string mn = "Print: ";
+	std::cout << this->ToString() << std::endl;
+}
+
+//ToString does the opposite of Parse: it assembles a string based on
+// the object's type data
+std::string SPXPlotType::ToString(void) {
+	std::string mn = "ToString: ";
+	
+	//Empty type
+	if(this->IsEmpty()) {
+		return "";
+	}
+	
+	//Check for validity
+	if(!this->IsValid()) {
+		return "INVALID_PLOT_TYPE";
+	}
+	
+	//Build syle option vector
+	std::vector<std::string> v;
+	
+	if(type & PT_DATA) {
+		v.push_back("data[]");
+	} else {
+		v.push_back("data");
+	}
+
+	if(type & PT_GRID) {
+		v.push_back("grid[]");
+	} else {
+		v.push_back("grid");
+	}
+
+	if(type & PT_PDF) {
+		v.push_back("pdf[]");
+	} else {
+		v.push_back("pdf");
+	}
+
+	return SPXStringUtilities::VectorToCommaSeparatedList(v);
+}
+
+//Determines the validity of the display type
+bool SPXPlotType::IsValid(void) {
+	std::string mn = "IsValid: ";
+	
+	if(type == PT_INVALID) {
+		if(debug) std::cout << cn << mn << "Plot type is invalid: Set to PT_INVALID (-1)" << std::endl;
+		return false;
+	}
+	
+	if(!IsType1() && !IsType2() && !IsType3() && !IsType4()) {
+		if(debug) std::cout << cn << mn << "Plot type is invalid: Not a known combination: " << type << std::endl;
+		return false;
+	}
+	
+	return true;
+}
+
---
+++ src/SPXPlotType.h
@@ -0,0 +1,192 @@
+//************************************************************/
+//
+//	Plot Type Header
+//
+//	Outlines the SPXPlotType class, which defines the four
+//	supported plot types:
+//		
+//		1)	data, grid, pdf
+//
+//			ONE data file
+//			ONE grid file
+//			ONE pdf file
+//
+//		2)	data[], grid[], pdf
+//
+//			MANY data files
+//			MANY grid files (1:1 w/ data files)
+//			ONE pdf file
+//
+//		3) 	data, grid, pdf[]
+//
+//			ONE data file
+//			ONE grid file
+//			MANY pdf files
+//
+//		4)	data, grid[], pdf
+//
+//			ONE data file
+//			MANY grid files
+//			ONE pdf file
+//
+//	@Author: 	J. Gibson, C. Embree, T. Carli - CERN ATLAS
+//	@Date:		27.10.2014
+//	@Email:		gibsjose@mail.gvsu.edu
+//
+//************************************************************/
+
+#ifndef SPXPLOTTYPE_H
+#define SPXPLOTTYPE_H
+
+//Bitfields for determining style type
+const int PT_INVALID =		-1;
+const int PT_DATA = 		(1 << 0);	//0b00000001
+const int PT_GRID = 		(1 << 1);	//0b00000010
+const int PT_PDF = 			(1 << 2);	//0b00000100
+
+//A '0' represents a SINGLE instance
+//A '1' represents MULTIPLE instances
+//i.e. data[], grid[], pdf will have the type:
+//			0b00000011  =  3
+//		   		   ^^^
+//		   		   |||
+// ONE PDF -------/ ||
+// MULT Grids -----/ |
+// MULT Data -------/
+
+class SPXPlotType {
+
+public:
+	SPXPlotType () : type(0) {}
+	explicit SPXPlotType(std::string s);
+	
+	void Parse(std::string s);
+	void Print(void);
+	std::string ToString(void);
+	bool IsEmpty(void);
+	bool IsValid(void);
+	
+	void Clear(void) {
+		type = 0;
+	}
+	
+	static bool GetDebug(void) {
+		return debug;
+	}
+	
+	static void SetDebug(bool b) {
+		debug = b;
+	}
+	
+	int GetType(void) {
+		return (int)type;
+	}
+
+	//Type 1 = Single Data, Single Grid, Single PDF
+	bool IsType1(void) {
+		if(ContainsSingleData() && ContainsSingleGrid() && ContainsSinglePDF()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool IsType2(void) {
+		if(ContainsMultipleData() && ContainsMultipleGrids() && ContainsSinglePDF()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool IsType3(void) {
+		if(ContainsSingleData() && ContainsSingleGrid() && ContainsMultiplePDFs()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool IsType4(void) {
+		if(ContainsSingleData() && ContainsMultipleGrids() && ContainsSinglePDF()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool ContainsSingleData(void) {
+		return ContainsSingle(PT_DATA);
+	}
+
+	bool ContainsSingleGrid(void) {
+		return ContainsSingle(PT_GRID);
+	}
+
+	bool ContainsSinglePDF(void) {
+		return ContainsSingle(PT_PDF);
+	}
+
+	bool ContainsMultipleData(void) {
+		return ContainsMultiple(PT_DATA);
+	}
+
+	bool ContainsMultipleGrids(void) {
+		return ContainsMultiple(PT_GRID);
+	}
+
+	bool ContainsMultiplePDFs(void) {
+		return ContainsMultiple(PT_PDF);
+	}
+
+	bool ContainsSingle(int mask) {
+		return !ContainsMultiple(mask);
+	}
+
+	bool ContainsMultiple(int mask) {
+		if(type & mask) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	bool Contains(int mask) {
+		//Invalid mask... Does not contain data, reference, or convolute
+		if(!(mask & (DS_OVERLAY | DS_RATIO))) {
+			return false;
+		}
+
+		if(((type & mask) == DS_OVERLAY) && (this->IsValid())) {
+			return true;
+		} else if(((type & mask) == DS_RATIO) && (this->IsValid())) {
+			return true;
+		} else {
+			return false;
+		}
+
+		return false;
+	}
+
+	bool ContainsOverlay(void) {
+		if((type & DS_OVERLAY) && this->IsValid()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	bool ContainsRatio(void) {
+		if((type & DS_RATIO) && this->IsValid()) {
+			return true;
+		}
+		
+		return false;
+	}
+
+private:
+	static bool debug;
+	unsigned int type;
+};
+
+#endif
