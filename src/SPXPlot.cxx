9599c62c8c17038cfd1032c616dc73ea5e81a816
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-11-03 11:06:42 +0100

    Debug

--- src/SPXPlot.cxx
+++ src/SPXPlot.cxx
@@ -600,7 +600,10 @@ void SPXPlot::DrawRatio(void) {
 
 		//Set x errors to zero if ratio involves convolute AND is not plot band
 		if(ratios[i].HasConvolute() && !steeringFile->GetPlotBand()) {
+
 			SPXGraphUtilities::ClearXErrors(ratios[i].GetRatioGraph());
+
+			if(debug) std::cout << cn << mn << "Set X errors to zero for ratios[" << i << "]" << std::endl;
 		}
 
 		if(ratios[i].IsDataStat() || ratios[i].IsDataTot()) {
