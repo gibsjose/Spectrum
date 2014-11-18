7a331baa4f97e76f9d448c52a390a76a13d04642
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-11-18 14:42:32 +0100

    Testing 20% DeltaMinMax

--- src/SPXGraphUtilities.h
+++ src/SPXGraphUtilities.h
@@ -25,7 +25,7 @@
 #define PERFORM_DELTA_MIN_MAX true
 
 #if PERFORM_DELTA_MIN_MAX
-const double DELTA_MIN_MAX = 0.10;	//Extra space on the graph for min/max: 0.10 = 10%
+const double DELTA_MIN_MAX = 0.20;	//Extra space on the graph for min/max: 0.10 = 10%
 #endif
 
 typedef enum DivideErrorType_t {
