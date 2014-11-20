9b5ad953c9512ee92c93d761018ab41b717434f8
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-11-20 10:29:29 +0100

    Testing pseudo- Alpha S and Scale Band plotting

--- src/SPXCrossSection.h
+++ src/SPXCrossSection.h
@@ -51,7 +51,7 @@ public:
 	}
 
 	//Pseudo-Method for returning Alpha S results: (Just scales PDF errors by 1/3)
-	TGraphAsymmErrors *GetAlphaSResults(void) {
+	TGraphAsymmErrors *GetAlphaSBandResults(void) {
 		//return pdf->h_AlphaS_results;
 		static TGraphAsymmErrors as;
 		as = *(pdf->h_PDFBand_results);
@@ -60,7 +60,7 @@ public:
 	}
 
 	//Pseudo-Method for returning Scale results: (Just scales PDF errors by 1/3)
-	TGraphAsymmErrors *GetScaleResults(void) {
+	TGraphAsymmErrors *GetScaleBandResults(void) {
 		static TGraphAsymmErrors sc;
 		sc = *(pdf->h_PDFBand_results);
 		SPXGraphUtilities::ScaleYErrors(&sc, (1.0/4.0));
