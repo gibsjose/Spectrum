debd70c55b2a210fe7ddc497dd327b58f1040561
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-12-04 16:11:06 +0100

    Properly naming cross section TGraphs

--- src/SPXCrossSection.cxx
+++ src/SPXCrossSection.cxx
@@ -67,9 +67,9 @@ void SPXCrossSection::Create(void) {
 	}
 
 	//Set the graph names
-	// if(pdf->h_PDFBand_results) pdf->h_PDFBand_results->SetName(pdfName);
-	// if(pdf->h_AlphaS_results) pdf->h_AlphaS_results->SetName(alphaSName);
-	// if(pdf->h_Scale_results) pdf->h_Scale_results->SetName(scaleName);
+	if(pdf->h_PDFBand_results) pdf->h_PDFBand_results->SetName(pdfName);
+	if(pdf->h_AlphaS_results) pdf->h_AlphaS_results->SetName(alphaSName);
+	if(pdf->h_Scale_results) pdf->h_Scale_results->SetName(scaleName);
 }
 
 void SPXCrossSection::ParseCorrections(void) {
