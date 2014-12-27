788c970d544ca8dacfde1847fb62240a59ca3a44
“tancredi <tancredi.carli@cern.ch>
2014-12-27 14:10:40 +0100

    updates in steering files

--- test_all.sh
+++ test_all.sh
@@ -6,21 +6,26 @@
 #
 # ATLAS inclusive jet cross section
 #
-# change steering file to formate t3s/t3a
+# ATLAS inclusive jets 2011 7 TeV
 ./Spectrum Steering/atlas_incljet2011.txt
-./Spectrum Steering/atlas_incljet2011_all.txt
+#./Spectrum Steering/atlas_incljet2011_all.txt
 #
+#
+# ATLAS inclusive jets 2011 2.76 TeV
+./Spectrum Steering/atlas_incljet2011_2p76TeV.txt
+#
+# ATLAS inclusive Z boson 2010 7 TeV
 ./Spectrum Steering/atlas_inclZ2010.txt
 #
 #./Spectrum Steering/atlas_topnjets.txt
 #
-#
-./Spectrum Steering/atlas_incljet2011_2p76TeV.txt
+# Top parton
 #
 ./Spectrum Steering/atlas_topparton_mtt.txt
 ./Spectrum Steering/atlas_topparton_ptttop.txt
 ./Spectrum Steering/atlas_topparton_ttbarpt.txt
 ./Spectrum Steering/atlas_topparton_ttbary.txt
-
-
 #
+# band test
+#
+./Spectrum Steering/test_band.txt
