d3aa7d5efd8b462fe01c2934857d28a3041986ba
Tancredi Carli <tancredi.carli@cern.ch>
2015-04-09 21:17:35 +0200

    copygrids includes 2010 data

--- copygrids.sh
+++ copygrids.sh
@@ -3,6 +3,7 @@
 export dir=/afs/cern.ch/user/a/applgrid/work/public
 export file=Grids/jet/atlas/incljets2010/nlojet/atlas-incljets-arxiv-1112.6297
 echo $dir/$file/ $file/.
+mkdir -p $file
 cp -r $dir/$file/ $file/.
 
 export file=Grids/jet/atlas/incljets2011/nlojet
