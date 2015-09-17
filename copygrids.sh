2501a8ab5b96625f57823df2653978e9b073cef2
Tancredi Carli <tancredi.carli@cern.ch>
2015-09-17 09:35:33 +0200

    update copygrid

--- copygrids.sh
+++ copygrids.sh
@@ -7,39 +7,57 @@ echo $dir/$file/ $file/.
 mkdir -p $file
 cp $dir/$file/*.root $file/.
 
+echo done
+echo
+
 export file=Grids/jet/atlas/incljets2010/nlojet/atlas-incljets-arxiv-1112.6297/r06
 echo $dir/$file/ $file/.
 mkdir -p $file
-cp $dir/$file/*.root $file/.
+cp -r $dir/$file/*.root $file/.
+
+echo done
+echo
 
 export file=Grids/jet/atlas/incljets2010/nlojet/pereventscale
 echo $dir/$file/ $file/.
 mkdir -p $file
 cp $dir/$file/*.root $file/.
 
-export file=Grids/jet/atlas/incljets2011/nlojet
+echo done
+echo
+
+export file=Grids/jet/atlas/incljets2011/nlojet/
 echo $dir/$file/*.root $file/.
 cp $dir/$file/*.root $file/.
 
+echo done
+echo
 
 export file=Grids/jet/atlas/incljets2011_2p76TeV/nlojet
 echo $dir/$file/*.root $file/
 cp $dir/$file/*.root $file/.
 
-
-export file=Grids/jet/atlas/incljets2010/nlojet
-echo $dir/$file/*.root $file/
-cp $dir/$file/*.root $file/.
+echo done
+echo
 
 
 export file=Grids/top/atlas/mcfm/ttbarresolvedpartons
 echo $dir/$file/*.root $file/
 cp $dir/$file/*.root $file/.
 
+echo done
+echo
+
 export file=Grids/Zincl/atlas/sherpa/
 echo $dir/$file/*.root $file/
 cp $dir/$file/*.root $file/.
 
+echo done
+echo
+
 export file=Grids/jet/atlas/dijets2011
 echo $dir/$file/*.root $file/
 cp $dir/$file/*.root $file/.
+
+echo done
+echo
